9be26c0621c9b2022d9b0c9ee5a06a3b5405143c
Osman Goni <osmancse32@gmail.com>
2026-03-05 14:47:05 +0600

    rabbit mq second stash

--- CommandsService/obj/Debug/net9.0/CommandsService.AssemblyInfo.cs
+++ CommandsService/obj/Debug/net9.0/CommandsService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50adaa95ab2093751f62822889c7e79c35b8904")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa82e89e5747ad8acb6285e4c30a8881fefea8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
